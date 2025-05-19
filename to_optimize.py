e668f9fa187a48d00b868bcc541eab87c700e421
DemenYunn <victorperkinsgru@gmail.com>
2025-05-19 13:24:23 -0700

    Update to_optimize.py

--- to_optimize.py
+++ to_optimize.py
@@ -1,10 +1,1 @@
-def implementation_3():
-    import numpy as np
-    counts = np.arange(0, 100000000)
-    print("Done")
-count = 0
-while True:
-    count += 1
-    if count == 100000000:
-        print("Done")
-        break
+# Put your code that is needed to be optimized here please :)
